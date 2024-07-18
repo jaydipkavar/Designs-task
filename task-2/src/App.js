ae5dc8034316a3dda9bb198c635f81c70eea1e1a
jaydipkavar <pateljaydeep1624@gmail.com>
2024-07-18 09:42:57 +0530

    feat:add react app task 2

---
+++ task-2/src/App.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import "./App.css";
+import Signup1 from "./components/Signup1";
+
+function App() {
+  return (
+    <>
+      <Signup1 />
+    </>
+  );
+}
+
+export default App;
