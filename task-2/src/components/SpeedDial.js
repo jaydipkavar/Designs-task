3cb6c5efc4c5acaa67b795e920ae22ae0446b63d
jaydipkavar <pateljaydeep1624@gmail.com>
2024-07-25 18:51:10 +0530

    fix:change side navabar

--- task-2/src/components/SpeedDial.js
+++ task-2/src/components/SpeedDial.js
@@ -4,89 +4,98 @@ import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 const SpeedDial = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const speedDialRef = useRef(null);
+ const [isOpen, setIsOpen] = useState(false);
+ const speedDialRef = useRef(null);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+ const toggleMenu = () => {
+   setIsOpen((prevState) => !prevState);
+ };
 
-  const handleLinkClick = () => {
-    if (isOpen) {
-      setIsOpen(false); // Close the menu when a link is clicked
-    }
-  };
+ const handleLinkClick = () => {
+   if (isOpen) {
+     setIsOpen(false); 
+   }
+ };
 
-  const handleClickOutside = (event) => {
-    if (speedDialRef.current && !speedDialRef.current.contains(event.target)) {
-      setIsOpen(false);
-    }
-  };
+ const handleClickOutside = (event) => {
+   if (
+     speedDialRef.current &&
+     !speedDialRef.current.contains(event.target) &&
+     !event.target.closest("button")
+   ) {
+     setIsOpen(false); 
+   }
+ };
 
-  useEffect(() => {
-    if (isOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside);
-    }
+ useEffect(() => {
+   if (isOpen) {
+     document.addEventListener("mousedown", handleClickOutside);
+   } else {
+     document.removeEventListener("mousedown", handleClickOutside);
+   }
 
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [isOpen]);
-
+   return () => {
+     document.removeEventListener("mousedown", handleClickOutside);
+   };
+ }, [isOpen]);
   return (
-    <div
-      ref={speedDialRef}
-      data-dial-init
-      className='fixed bottom-6 start-6 group z-10 flex justify-start flex-col items-start w-1/3'
-    >
+    <div>
       <div
-        id='speed-dial-menu-bottom-left'
-        className={`flex flex-col items-center ${
-          isOpen ? "mb-4" : "hidden"
-        } space-y-2`}
+        ref={speedDialRef}
+        className={`fixed bottom-20 left-0 z-10 flex flex-col transition-transform transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+        style={{
+          width: "auto",
+          height: "auto",
+          backgroundColor: "white",
+          boxShadow: "0 -4px 8px rgba(0,0,0,0.2)",
+          transition: "transform 0.3s ease",
+          marginBottom: "20px",
+          textAlign:"center"
+        }}
       >
-        <ul className='speeddialbox'>
-          <Link to='/signup1'>
-            <li onClick={handleLinkClick}>Signup1</li>
-          </Link>
-          <Link to='/signup2'>
-            <li onClick={handleLinkClick}>Signup2</li>
-          </Link>
-          <Link to='/signup3'>
-            <li onClick={handleLinkClick}>Signup3</li>
-          </Link>
-          <Link to='/signup4'>
-            <li onClick={handleLinkClick}>Signup4</li>
-          </Link>
-          <Link to='/signup5'>
-            <li onClick={handleLinkClick}>Signup5</li>
-          </Link>
-          <Link to='/login1'>
-            <li onClick={handleLinkClick}>Login1</li>
-          </Link>
-          <Link to='/login2'>
-            <li onClick={handleLinkClick}>Login2</li>
-          </Link>
-          <Link to='/login3'>
-            <li onClick={handleLinkClick}>Login3</li>
-          </Link>
-          <Link to='/login4'>
-            <li onClick={handleLinkClick}>Login4</li>
-          </Link>
-          <Link to='/login5'>
-            <li onClick={handleLinkClick}>Login5</li>
-          </Link>
-        </ul>
+        <div className='flex flex-col p-4 space-y-2 h-full'>
+          <ul className='flex flex-col space-y-2'>
+            <Link to='/signup1' onClick={handleLinkClick}>
+              <li className='p-2 hover:bg-gray-200 rounded'>Signup1</li>
+            </Link>
+            <Link to='/signup2' onClick={handleLinkClick}>
+              <li className='p-2 hover:bg-gray-200 rounded'>Signup2</li>
+            </Link>
+            <Link to='/signup3' onClick={handleLinkClick}>
+              <li className='p-2 hover:bg-gray-200 rounded'>Signup3</li>
+            </Link>
+            <Link to='/signup4' onClick={handleLinkClick}>
+              <li className='p-2 hover:bg-gray-200 rounded'>Signup4</li>
+            </Link>
+            <Link to='/signup5' onClick={handleLinkClick}>
+              <li className='p-2 hover:bg-gray-200 rounded'>Signup5</li>
+            </Link>
+            <Link to='/login1' onClick={handleLinkClick}>
+              <li className='p-2 hover:bg-gray-200 rounded'>Login1</li>
+            </Link>
+            <Link to='/login2' onClick={handleLinkClick}>
+              <li className='p-2 hover:bg-gray-200 rounded'>Login2</li>
+            </Link>
+            <Link to='/login3' onClick={handleLinkClick}>
+              <li className='p-2 hover:bg-gray-200 rounded'>Login3</li>
+            </Link>
+            <Link to='/login4' onClick={handleLinkClick}>
+              <li className='p-2 hover:bg-gray-200 rounded'>Login4</li>
+            </Link>
+            <Link to='/login5' onClick={handleLinkClick}>
+              <li className='p-2 hover:bg-gray-200 rounded'>Login5</li>
+            </Link>
+          </ul>
+        </div>
       </div>
       <button
         type='button'
-        data-dial-toggle='speed-dial-menu-bottom-left'
-        aria-controls='speed-dial-menu-bottom-left'
+        aria-controls='speed-dial-menu'
         aria-expanded={isOpen}
         onClick={toggleMenu}
-        className='flex items-center justify-center text-white bg-blue-700 rounded-full w-14 h-14 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800'
+        className='fixed bottom-6 left-6 z-20 bg-blue-700 text-white rounded-full w-14 h-14 flex items-center justify-center shadow-lg'
       >
         <svg
           className={`w-5 h-5 transition-transform ${
@@ -105,7 +114,7 @@ const SpeedDial = () => {
             d='M9 1v16M1 9h16'
           />
         </svg>
-        <span className='sr-only'>Open actions menu</span>
+        <span className='sr-only'>Toggle drawer</span>
       </button>
     </div>
   );
