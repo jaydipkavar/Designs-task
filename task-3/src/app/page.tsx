dc3286ac664021854891ddf1e02d4344477a361a
jaydipkavar <pateljaydeep1624@gmail.com>
2024-07-30 09:43:27 +0530

    fix:change redirect

--- task-3/src/app/page.tsx
+++ task-3/src/app/page.tsx
@@ -1,9 +1,17 @@
 /** @format */
-
-import Image from "next/image";
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation"; // Updated import for next/navigation
 import Sidebar from "./components/sidebar";
 import "../app/globals.css";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/score");
+  }, [router]);
+
   return (
     <>
       <Sidebar />
